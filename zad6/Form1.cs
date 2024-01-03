5cfa30c40ab010444e28cefd0aa26253dad13a37
Alina Upyrova <115549868+alinaupyrova@users.noreply.github.com>
2024-01-03 09:30:00 +0100

    Add files via upload

---
+++ zad6/Form1.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+
+namespace zad6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class graf
+    {
+        ArrayList listaWezel = new ArrayList();
+        ArrayList listaKrawedzi = new ArrayList();
+        public void znajdzSasiadzi()
+        {
+            var kopiaListy = this.listaWezel.ToList();
+
+
+
+
+        }
+        public wezel4 SzukajMinimum(Dictionary<wezel4,Element> tabelka Lista<wezel4> Q)
+        {
+            int min = tabelka[Q[0]];
+            while(zbiorQ != null)
+            {
+
+            }
+        }
+    }
+    public class wezel4
+    {
+        int wartosc;
+        ArrayList listaKrawedzi = new ArrayList();
+    }
+    public class krawedzi
+    {
+        int waga;
+        int poczatek;
+        int koniec;
+    }
+    
+   
+}
